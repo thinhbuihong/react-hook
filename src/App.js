560d29f780ac8b20622af471a9766db4c83443d2
thinhbuihong <thinhbuihong.ma@gmail.com>
2020-10-06 15:11:12 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import Example from './Example';
+
+function App() {
+  return (
+    <div className="app">
+      app
+    </div>
+  );
+}
+
+export default App;
